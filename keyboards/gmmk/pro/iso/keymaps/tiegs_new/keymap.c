075232d49aeab3b31f726de9b6cd9071ed017f24
Tilman Eggers <dev@tilmaneggers.de>
2023-02-15 22:39:26 +0100

    Add knob media controls

--- keyboards/gmmk/pro/iso/keymaps/tiegs_new/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/tiegs_new/keymap.c
@@ -120,11 +120,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (clockwise) {
-      tap_code(KC_VOLU);
-    } else {
-      tap_code(KC_VOLD);
+
+    uint8_t layer = get_highest_layer(layer_state);
+
+    if (layer == _AL ) {
+      if (clockwise) {
+        tap_code(KC_MNXT);
+      } else {
+        tap_code(KC_MPRV);
+      }
     }
+    else {
+      if (clockwise) {
+        tap_code(KC_VOLU);
+      } else {
+        tap_code(KC_VOLD);
+      }
+    }
+
     return true;
 }
 #endif
